d038bc641fadeca97e6695d3166d3f489c419fad
SandorBoss <boszormenyi.s@gmail.com>
2023-04-02 11:13:07 +0200

    storage is working

--- spec/storage.spec.js
+++ spec/storage.spec.js
@@ -4,19 +4,47 @@ const storage = new Storage();
 const Constants = require('./test-constants.js');
 const testConsts = new Constants();
 
-describe("storage is working", () => {
+describe("storage", () => {
 
+    it("should return query result as json", () => {
+        let answerJson = storage.queryInFileSystem();
+        expect(answerJson).toEqual(
+            {
+                port: '8080',
+                cpuUsage: '65%',
+                freeMemory: '80gb'
+            }
+        );
+    });
+
+    it("should parse file content with port to json", () => {
+        let fileContent = storage.getFileContent(testConsts.testPort);
+        let answerJson = storage.parseAnswerToJson(
+            testConsts.testPort,
+            fileContent
+        );
+        expect(answerJson).toEqual(
+            {
+                port: '8080',
+                cpuUsage: '65%',
+                freeMemory: '80gb'
+            }
+        );
+    });
+    
     it("should be json with port", () => {
         expect(storage.portFromCore).toEqual(testConsts.testJson);
     });
 
-    it("should be a string number", () => {
-        expect(storage.portToQuery).toEqual(testConsts.testPort);
+    it("should parse port to string", () => {
+        let portFromCore = storage.portFromCore;
+        let portAsString = storage.parsePortToString();
+        expect(portAsString).toEqual('8080');
     });
 
     it("should get file content", () => {
         let fileContent = storage.getFileContent(testConsts.testPort);
         expect(fileContent).toEqual(testConsts.fileContent);
-    }) 
+    }); 
 
 });
--- storage.js
+++ storage.js
@@ -8,6 +8,30 @@ module.exports = class Storage {
 
     portToQuery = this.portFromCore.port.toString();
 
+    queryInFileSystem() {
+        let portToQuery = this.parsePortToString();
+        let plainFileContent = this.getFileContent(portToQuery);
+        let queryAnswer = this.parseAnswerToJson(
+            portToQuery,
+            plainFileContent
+        );
+
+        return queryAnswer;
+    }
+    
+    parsePortToString() {
+        return this.portFromCore.port.toString();
+    }
+
+    parseAnswerToJson(portString, fileContent) {
+        let arrayFromFileContent = fileContent.split('\n');
+        return {
+            port: portString,
+            cpuUsage: arrayFromFileContent[0],
+            freeMemory: arrayFromFileContent[1]
+        }
+    }
+    
     getFileContent(portString) {
         let fileContent = fileSystem.readFileSync(
             `./server-infos/${portString}.txt`,
